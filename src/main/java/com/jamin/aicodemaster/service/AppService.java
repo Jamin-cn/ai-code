d7187ed9bed4819ac1a0447b5aef93cce7d66de4
Jamin <your-email@example.com>
2026-02-21 18:25:17 +0800

    完成应用模块基本功能

--- src/main/java/com/jamin/aicodemaster/service/AppService.java
+++ src/main/java/com/jamin/aicodemaster/service/AppService.java
@@ -1,7 +1,12 @@
 package com.jamin.aicodemaster.service;
 
+import com.jamin.aicodemaster.model.dto.app.AppQueryRequest;
+import com.jamin.aicodemaster.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.jamin.aicodemaster.model.entity.App;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -10,4 +15,9 @@ import com.jamin.aicodemaster.model.entity.App;
  */
 public interface AppService extends IService<App> {
 
+    AppVO getAppVO(App app);
+
+    List<AppVO> getAppVOList(List<App> appList);
+
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 }
